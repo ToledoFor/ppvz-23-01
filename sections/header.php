817dd0f570e5f9b6e7bff4d74d513d177e2b5e17
Dmitriy Olhovsky <dmitriy.olhovsky@beetroot.se>
2023-04-06 10:43:49 +0300

    Add basic template structure

---
+++ sections/header.php
@@ -0,0 +1,7 @@
+<h1>This is the page header!</h1>
+
+<ul>
+    <li><a href="/ppvz-2023/practice-03/?task=1">Task 01</a></li>
+    <li><a href="/ppvz-2023/practice-03/?task=2">Task 02</a></li>
+    <li><a href="/ppvz-2023/practice-03/?task=3">Task 03</a></li>
+</ul>
